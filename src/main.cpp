a7d606a13cb6148beb6e8d7bcacf452b4296c0fe
Joshua Rehak <jsrehak@gmail.com>
2018-05-08 13:31:39 -0700

    created initial main function

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#ifdef TEST
+#include <gtest/gtest.h>
+#endif
+
+int main(int argc, char* argv[]) {
+#ifdef TEST
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+#else
+  std::cout << "XPT with Protocol Buffers!" <<std::endl;
+  return 0;
+#endif
+}
