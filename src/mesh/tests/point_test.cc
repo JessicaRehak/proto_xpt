7c3dbf52b5bfde1d74b2f3e764afbd453548f445
Joshua Rehak <jsrehak@gmail.com>
2018-05-18 10:27:51 -0700

    removed hard coding of x and y values in tests of point

--- src/mesh/tests/point_test.cc
+++ src/mesh/tests/point_test.cc
@@ -9,23 +9,26 @@
 class PointTest : public ::testing::Test {
  protected:
   void SetUp() override {};
-  xpt::mesh::Point testPoint{1.0, -10.0};
-  xpt::mesh::Point testPoint_gt{3.4, 0.0};
-  xpt::mesh::Point testPoint_lt{0.3, -20.0};
-  xpt::mesh::Point testPoint_nt{3.0, -40.5}; //not gt or lt
-  xpt::mesh::Point testPoint_nt_2{-45.0, 3.5}; //not gt or lt
-  xpt::mesh::Point testPoint_eq{1.0, -10.0};
+  float x = 1.0, y = -10.0;
+  float gt_x = 2.23*x, lt_x = 0.534*x;
+  float gt_y = -2.394*y, lt_y = 10.56*y;
+  xpt::mesh::Point testPoint{x, y};
+  xpt::mesh::Point testPoint_gt{gt_x, gt_y};
+  xpt::mesh::Point testPoint_lt{lt_x, lt_y};
+  xpt::mesh::Point testPoint_nt{gt_x, lt_y}; //not gt or lt
+  xpt::mesh::Point testPoint_nt_2{lt_x, gt_y}; //not gt or lt
+  xpt::mesh::Point testPoint_eq{x, y};
   xpt::mesh::Point testPoint_ne{3.4, 0.0};
 };
 
 TEST_F(PointTest, PairConstructor) {
-  std::pair<float, float> position{1.0, -4.0};
+  xpt::mesh::Coordinate position{1.0, -4.0};
   xpt::mesh::Point testPoint_c{position};
   ASSERT_EQ(testPoint_c.position_, position);
  }
 
 TEST_F(PointTest, FloatConstructor) {
-  std::pair<float, float> position{1.0, -4.0};
+  xpt::mesh::Coordinate position{1.0, -4.0};
   xpt::mesh::Point testPoint_c{1.0, -4.0};
   ASSERT_EQ(testPoint_c.position_, position);
 }
@@ -36,8 +39,8 @@ TEST_F(PointTest, Length) {
 }
 
 TEST_F(PointTest, Accessors) {
-  std::pair<float, float> ans{3.0, -10.0};
-  std::pair<float, float> ans2{3.0, 12.7};
+  xpt::mesh::Coordinate ans{3.0, -10.0};
+  xpt::mesh::Coordinate ans2{3.0, 12.7};
   
   testPoint.x_ = 3.0;
   ASSERT_EQ(testPoint.position_, ans);
@@ -95,25 +98,21 @@ TEST_F(PointTest, ComparisonEqOps) {
 }
 
 TEST_F(PointTest, PlusEqOpPair) {
-  std::pair<float, float> p2{10.0, 20.0};
-  std::pair<float, float> ans{testPoint.position_.first + p2.first,
-        testPoint.position_.second + p2.second};
-  std::pair<float, float> ans_2{testPoint.position_.first + 3*p2.first,
-        testPoint.position_.second + 3*p2.second};
+  xpt::mesh::Coordinate p2{10.0, 20.0};
+  xpt::mesh::Coordinate ans{x + p2.first, y + p2.second};
+  xpt::mesh::Coordinate ans_2{x + 3*p2.first, y + 3*p2.second};
+  
   testPoint += p2;
   ASSERT_EQ(testPoint.position_, ans);
-  // Check chaining
   (testPoint += p2) += p2;
   ASSERT_EQ(testPoint.position_, ans_2);
 }
 
 TEST_F(PointTest, PlusEqOpPoint) {
-  std::pair<float, float> p2{10.0, 20.0};
+  xpt::mesh::Coordinate p2{10.0, 20.0};
   xpt::mesh::Point point2{p2};
-  std::pair<float, float> ans{testPoint.position_.first + p2.first,
-        testPoint.position_.second + p2.second};
-  std::pair<float, float> ans_2{testPoint.position_.first + 3*p2.first,
-        testPoint.position_.second + 3*p2.second};
+  xpt::mesh::Coordinate ans{x + p2.first, y + p2.second};
+  xpt::mesh::Coordinate ans_2{x + 3*p2.first, y + 3*p2.second};
   testPoint += point2;
   ASSERT_EQ(testPoint.position_, ans);
   // Check chaining
@@ -122,11 +121,9 @@ TEST_F(PointTest, PlusEqOpPoint) {
 }
 
 TEST_F(PointTest, PlusOpPair) {
-  std::pair<float, float> p2{10.0, 20.0};
-  std::pair<float, float> ans{testPoint.position_.first + p2.first,
-        testPoint.position_.second + p2.second};
-  std::pair<float, float> ans_2{testPoint.position_.first + 2*p2.first,
-        testPoint.position_.second + 2*p2.second};
+  xpt::mesh::Coordinate p2{10.0, 20.0};
+  xpt::mesh::Coordinate ans{x + p2.first, y + p2.second};
+  xpt::mesh::Coordinate ans_2{x + 2*p2.first, y + 2*p2.second};
 
   xpt::mesh::Point new_point = testPoint + p2;
   xpt::mesh::Point chain_point = (testPoint + p2) + p2;
@@ -136,12 +133,10 @@ TEST_F(PointTest, PlusOpPair) {
 }
 
 TEST_F(PointTest, PlusOpPoint) {
-  std::pair<float, float> p2{10.0, 20.0};
+  xpt::mesh::Coordinate p2{10.0, 20.0};
   xpt::mesh::Point point2{p2};
-  std::pair<float, float> ans{testPoint.position_.first + p2.first,
-        testPoint.position_.second + p2.second};
-  std::pair<float, float> ans_2{testPoint.position_.first + 2*p2.first,
-        testPoint.position_.second + 2*p2.second};
+  xpt::mesh::Coordinate ans{x +  p2.first, y + p2.second};
+  xpt::mesh::Coordinate ans_2{x + 2*p2.first, y + 2*p2.second};
 
   xpt::mesh::Point new_point = testPoint + point2;
   xpt::mesh::Point chain_point = (testPoint + point2) + point2;
