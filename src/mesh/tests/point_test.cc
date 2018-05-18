106e982da2d8edf327377830af995398b66dc8f2
Joshua Rehak <jsrehak@gmail.com>
2018-05-18 10:31:54 -0700

    moved comparison tests for point into a second test fixture

--- src/mesh/tests/point_test.cc
+++ src/mesh/tests/point_test.cc
@@ -10,9 +10,13 @@ class PointTest : public ::testing::Test {
  protected:
   void SetUp() override {};
   float x = 1.0, y = -10.0;
+  xpt::mesh::Point testPoint{x, y};
+};
+
+class PointCompareTest : public PointTest {
+ protected:
   float gt_x = 2.23*x, lt_x = 0.534*x;
   float gt_y = -2.394*y, lt_y = 10.56*y;
-  xpt::mesh::Point testPoint{x, y};
   xpt::mesh::Point testPoint_gt{gt_x, gt_y};
   xpt::mesh::Point testPoint_lt{lt_x, lt_y};
   xpt::mesh::Point testPoint_nt{gt_x, lt_y}; //not gt or lt
@@ -56,14 +60,14 @@ TEST_F(PointTest, toStr_specific) {
   ASSERT_EQ(testPoint.to_str(4), "(1.0000, -10.0000)");
 }
 
-TEST_F(PointTest, EquivOps) {
+TEST_F(PointCompareTest, EquivOps) {
   ASSERT_TRUE(testPoint == testPoint_eq);
   ASSERT_FALSE(testPoint == testPoint_ne);
   ASSERT_TRUE(testPoint != testPoint_ne);
   ASSERT_FALSE(testPoint != testPoint_eq);
 }
 
-TEST_F(PointTest, EquivFloat) {
+TEST_F(PointCompareTest, EquivFloat) {
   float ans = std::sqrt(101);
   ASSERT_TRUE(testPoint == ans);
   ASSERT_FALSE(testPoint != ans);
@@ -71,7 +75,7 @@ TEST_F(PointTest, EquivFloat) {
   ASSERT_TRUE(testPoint != 34.2);
 }
 
-TEST_F(PointTest, ComparisonOps) {
+TEST_F(PointCompareTest, ComparisonOps) {
   ASSERT_TRUE(testPoint_gt > testPoint);
   ASSERT_TRUE(testPoint_lt < testPoint);
   ASSERT_FALSE(testPoint_gt < testPoint);
@@ -84,7 +88,7 @@ TEST_F(PointTest, ComparisonOps) {
   ASSERT_FALSE(testPoint_nt_2 < testPoint);
 }
 
-TEST_F(PointTest, ComparisonEqOps) {
+TEST_F(PointCompareTest, ComparisonEqOps) {
   ASSERT_TRUE(testPoint_gt >= testPoint);
   ASSERT_TRUE(testPoint_lt <= testPoint);
   ASSERT_FALSE(testPoint_gt <= testPoint);
